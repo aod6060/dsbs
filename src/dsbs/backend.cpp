d019bad8a024d1ce0e55a3be07f90483df53e74f
derf6060 <derf6060@derfnet.attlocal.net>
2023-06-20 01:04:23 -0500

    Finished loadSolutionProfile. Now its time for create source/object files.

--- src/dsbs/backend.cpp
+++ src/dsbs/backend.cpp
@@ -14,13 +14,51 @@ namespace be {
         return loadSolutionProfile(op, solutionFile, projectName);
     }
 
+    std::function<void(std::ifstream&)> loadProfile(core::profile::Profile& profile) {
+        return [&](std::ifstream& in) {
+            Json::Value root;
+            in >> root;
 
-    int loadSolutionProfile(core::Operation op, std::string solutionFile, std::string projectName) {
+            profile.type = root["type"].asString();
+            profile.version = root["version"].asInt();
 
-        // Load Solution
-        core::solution::Solution solution;
+            if(profile.type != "profile") {
+                std::cout << "Error: type isn't profile will try to load" << "\n";
+            }
 
-        util::loadStream(solutionFile, [&](std::ifstream& in) {
+            if(profile.version != PROFILE_FILE_VERSION) {
+                std::cout << "Error: version isn't correct will try to load" << "\n";
+            }
+
+            // Program
+            profile.programs.compiler = root["programs"]["compiler"].asString();
+            profile.programs.archiver = root["programs"]["archiver"].asString();
+            profile.programs.linker = root["programs"]["linker"].asString();
+
+
+            // File Extensions
+            // Source File Extensions
+            util::jsonIteratorString(root["file-extensions"]["source-file-extensions"], [&](std::string ext) {
+                profile.fileExtensions.sourceFileExtensions.push_back(ext);
+            });
+            profile.fileExtensions.objectFileExtension = root["file-extensions"]["object-file-extension"].asString();
+            profile.fileExtensions.binaryFileExtension = root["file-extensions"]["binary-file-extension"].asString();
+            profile.fileExtensions.staticLibExtension = root["file-extensions"]["static-lib-extension"].asString();
+            profile.fileExtensions.sharedLibExtension = root["file-extensions"]["shared-lib-extension"].asString();
+
+            // Flags
+            profile.flags.includeDirFlag = root["flags"]["include-dir-flag"].asString();
+            profile.flags.libDirFlag = root["flags"]["lib-dir-flag"].asString();
+            profile.flags.compileFlag = root["flags"]["compile-flag"].asString();
+            profile.flags.objectFlag = root["flags"]["object-flag"].asString();
+            profile.flags.staticLibFlags = root["flags"]["static-lib-flag"].asString();
+            profile.flags.sharedLibFlags = root["flags"]["shared-lib-flag"].asString();
+
+        };
+    }
+
+    std::function<void(std::ifstream&)> loadSolution(core::solution::Solution& solution) {
+        return [&](std::ifstream& in) {
             Json::Value root;
             in >> root;
 
@@ -45,8 +83,11 @@ namespace be {
 
                 project.name = v["name"].asString();
                 project.type = util::toType(v["type"].asString());
-                project.profile = v["profile"].asString();
-                
+                //project.profile = v["profile"].asString();
+                // Handle Profile
+                util::loadStream(v["profile"].asString(), loadProfile(project.profile));
+
+
                 // Directories
                 project.binDir = v["bin-dir"].asString();
 
@@ -74,7 +115,7 @@ namespace be {
                 });
 
                 util::jsonIteratorString(v["archiver-options"], [&](std::string str) {
-                    project.archiverOptions.push_back(str)
+                    project.archiverOptions.push_back(str);
                 });
 
                 util::jsonIteratorString(v["linker-options"], [&](std::string str) {
@@ -84,7 +125,15 @@ namespace be {
                 solution.projects.push_back(project);
             });
 
-        });
+        };
+    }
+
+    int loadSolutionProfile(core::Operation op, std::string solutionFile, std::string projectName) {
+
+        // Load Solution
+        core::solution::Solution solution;
+
+        util::loadStream(solutionFile, loadSolution(solution));
 
         //// Load Profile
         //core::profile::Profile profile;
