d924b504b6bf94fa01954ef49e4689834ce542dd
derf6060 <derf6060@derfnet.attlocal.net>
2023-07-05 00:41:15 -0500

    Clean All for dsbs is working perfectly.

--- src/dsbs/backend.cpp
+++ src/dsbs/backend.cpp
@@ -541,7 +541,40 @@ namespace be {
     }
 
     int cleanOperation(std::string projectName, core::solution::Solution& solution) {
-        std::cout << "Cleaning..." << "\n";
+        std::vector<std::filesystem::path> paths;
+
+        if(!projectName.empty()) {
+
+        } else {
+            std::for_each(solution.projects.begin(), solution.projects.end(), [&](core::solution::Project& project) {
+                // Scan through objects
+                std::for_each(project.sources.begin(), project.sources.end(), [&](core::solution::Source& source) {
+                    util::iterateDirectory(source.objDir, [&](std::filesystem::directory_entry entry) {
+                        if(entry.path().extension() == project.profile.fileExtensions.objectFileExtension) {
+                            paths.push_back(entry.path());
+                        }
+                    });
+
+                    if(project.type == core::solution::ProjectType::PT_EXECUTABLE) {
+                        std::stringstream cb;
+                        cb << project.binDir << project.name << project.profile.fileExtensions.binaryFileExtension;
+                        paths.push_back(std::filesystem::path(cb.str()));
+
+                    } else if(project.type == core::solution::ProjectType::PT_SHARED_LIB || project.type == core::solution::ProjectType::PT_STATIC_LIB) {
+                        std::stringstream cb;
+                        cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
+                        paths.push_back(std::filesystem::path(cb.str()));
+                    }
+                });
+            });
+        }
+
+        // Remove files
+        std::for_each(paths.begin(), paths.end(), [&](std::filesystem::path p) {
+            std::cout << "Removing: " << p.string() << "\n";
+            std::filesystem::remove(p);
+        });
+
         return 0;
     }
 
