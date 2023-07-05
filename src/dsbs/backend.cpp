4011f38c9ded2b542fb227af21dcc631b6f4ddf5
derf6060 <derf6060@derfnet.attlocal.net>
2023-07-04 22:46:15 -0500

    DSBS now supports single project building... Time to get started on cleaning the objects.

--- src/dsbs/backend.cpp
+++ src/dsbs/backend.cpp
@@ -166,9 +166,185 @@ namespace be {
         std::vector<core::command> commandList;
 
         if(!projectName.empty()) {
-            std::cout << "TODO: Create single project build" << "\n";
-        } else {
+            std::for_each(solution.projects.begin(), solution.projects.end(), [&](core::solution::Project& project) {
+                if(project.name == projectName) {
+                    // Grab sources
+                    std::cout << "Building: " << project.name << "\n";
 
+                    std::vector<std::string> objects;
+
+                    std::for_each(project.sources.begin(), project.sources.end(), [&](core::solution::Source& source) {
+
+                        
+
+                        // Iterate through directory and searching for correct source files
+                        util::iterateDirectory(std::filesystem::path(source.srcDir), [&](std::filesystem::directory_entry entry) {
+                            //std::cout << entry.path().string() << "\n";
+                            std::for_each(
+                                project.profile.fileExtensions.sourceFileExtensions.begin(),
+                                project.profile.fileExtensions.sourceFileExtensions.end(),
+                                [&](std::string ext) {
+                                    if(ext == entry.path().extension()) {
+                                        //std::cout << entry.path().string() << "\n";
+                                        core::solution::Source pairs;
+                                        pairs.srcDir = entry.path().string();
+
+                                        std::string temp = entry.path().filename().string();
+                                        std::vector<std::string> args;
+
+                                        util::strSplit(temp, '.', [&](std::string s) {
+                                            args.push_back(s);
+                                        });
+
+                                        std::stringstream ss;
+                                        ss << source.objDir << args[0] << project.profile.fileExtensions.objectFileExtension;
+
+                                        pairs.objDir = ss.str();
+
+                                        objects.push_back(pairs.objDir);
+
+                                        //std::cout << pairs.srcDir << " -> " << pairs.objDir << "\n";
+                                        // Build Command
+                                        std::stringstream command;
+                                        command << project.profile.programs.compiler << " ";
+                                        if(project.type == core::solution::ProjectType::PT_SHARED_LIB) {
+                                            //command << project.profile.flags.sharedLibFlags << " ";
+                                        }
+                                        command << project.profile.flags.compileFlag << " ";
+                                        command << pairs.srcDir << " ";
+                                        command << project.profile.flags.objectFlag << " ";
+                                        command << pairs.objDir << " ";
+                                        // Includes
+                                        std::for_each(project.includeDirs.begin(), project.includeDirs.end(), [&](std::string inc) {
+                                            command << project.profile.flags.includeDirFlag << inc << " ";
+                                        });
+                                        // Compiler Options
+                                        std::for_each(project.compilerOptions.begin(), project.compilerOptions.end(), [&](std::string option) {
+                                            command << option << " ";
+                                        });
+
+                                        //commandList.push_back(command.str());
+                                        core::command c;
+                                        c.command = command.str();
+                                        c.projectName = project.name;
+                                        commandList.push_back(c);
+                                    }
+                                }
+                            );
+                        });                    
+                    });
+
+                    // Build Project Command
+                    if(project.type == core::solution::ProjectType::PT_EXECUTABLE) {
+                        std::stringstream cb;
+
+                        cb << project.profile.programs.linker << " ";
+                        
+                        // Adding in objects
+                        std::for_each(objects.begin(), objects.end(), [&](std::string o) {
+                            cb << o << " ";
+                        });
+
+                        // Create Binary
+                        cb << project.profile.flags.objectFlag << " ";
+                        cb << project.binDir << project.name << project.profile.fileExtensions.binaryFileExtension << " ";
+
+                        // Link Libraries
+                        // Library Dirs
+                        std::for_each(project.libDirs.begin(), project.libDirs.end(), [&](std::string s) {
+                            cb << project.profile.flags.libDirFlag << s << " ";
+                        });
+
+                        // Libraries
+                        std::for_each(project.libs.begin(), project.libs.end(), [&](std::string s) {
+                            cb << project.profile.flags.libFlags << s << " ";
+                        });
+
+                        // Linker Options
+                        std::for_each(project.linkerOptions.begin(), project.linkerOptions.end(), [&](std::string s) {
+                            cb << s << " ";
+                        });
+
+                        core::command c;
+                        c.command = cb.str();
+                        c.projectName = project.name;
+
+                        commandList.push_back(c);
+
+                    } else if(project.type == core::solution::ProjectType::PT_STATIC_LIB) {
+                        std::stringstream cb;
+
+                        // ar [options] [archive-name] [objects]
+                        cb << project.profile.programs.archiver << " ";
+                        cb << project.profile.flags.staticLibFlags << " ";
+                        cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension << " ";
+                        // Adding in objects
+                        std::for_each(objects.begin(), objects.end(), [&](std::string o) {
+                            cb << o << " ";
+                        });
+                        core::command c;
+                        c.command = cb.str();
+                        c.projectName = project.name;
+                        commandList.push_back(c);
+                    } else if(project.type == core::solution::ProjectType::PT_SHARED_LIB) {
+
+                        std::cout << "TODO: Currently dsbs doesn't support shared but will in a future update. " << "\n";
+                        
+                        std::stringstream cb;
+
+                        // ar [options] [archive-name] [objects]
+                        cb << project.profile.programs.archiver << " ";
+                        cb << project.profile.flags.staticLibFlags << " ";
+                        cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension << " ";
+                        // Adding in objects
+                        std::for_each(objects.begin(), objects.end(), [&](std::string o) {
+                            cb << o << " ";
+                        });
+                        core::command c;
+                        c.command = cb.str();
+                        c.projectName = project.name;
+                        commandList.push_back(c);
+                        /*
+                        std::stringstream cb;
+
+                        cb << project.profile.programs.linker << " ";
+                        cb << project.profile.flags.sharedLibFlagsLinker << " ";
+
+                        // Adding in objects
+                        std::for_each(objects.begin(), objects.end(), [&](std::string o) {
+                            cb << o << " ";
+                        });
+
+                        // Create Binary
+                        cb << project.profile.flags.objectFlag << " ";
+                        cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension << " ";
+
+                        // Link Libraries
+                        // Library Dirs
+                        std::for_each(project.libDirs.begin(), project.libDirs.end(), [&](std::string s) {
+                            cb << project.profile.flags.libDirFlag << s << " ";
+                        });
+
+                        // Libraries
+                        std::for_each(project.libs.begin(), project.libs.end(), [&](std::string s) {
+                            cb << project.profile.flags.libFlags << s << " ";
+                        });
+
+                        // Linker Options
+                        std::for_each(project.linkerOptions.begin(), project.linkerOptions.end(), [&](std::string s) {
+                            cb << s << " ";
+                        });
+
+                        core::command c;
+                        c.command = cb.str();
+                        c.projectName = project.name;
+
+                        commandList.push_back(c);
+                        */
+                    }
+                }
+            });
+        } else {
             std::for_each(solution.projects.begin(), solution.projects.end(), [&](core::solution::Project& project) {
                 // Grab sources
                 std::cout << "Building: " << project.name << "\n";
@@ -210,7 +386,7 @@ namespace be {
                                     std::stringstream command;
                                     command << project.profile.programs.compiler << " ";
                                     if(project.type == core::solution::ProjectType::PT_SHARED_LIB) {
-                                        command << project.profile.flags.sharedLibFlags << " ";
+                                        //command << project.profile.flags.sharedLibFlags << " ";
                                     }
                                     command << project.profile.flags.compileFlag << " ";
                                     command << pairs.srcDir << " ";
@@ -291,7 +467,7 @@ namespace be {
                 } else if(project.type == core::solution::ProjectType::PT_SHARED_LIB) {
 
                     std::cout << "TODO: Currently dsbs doesn't support shared but will in a future update. " << "\n";
-                    
+
                     std::stringstream cb;
 
                     // ar [options] [archive-name] [objects]
