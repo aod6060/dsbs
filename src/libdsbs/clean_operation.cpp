fc0a78d858443869ff271be92250aa07ced2a324
derf6060 <derf6060@derfnet.attlocal.net>
2023-07-12 17:30:29 -0500

    Refactored clean_operation.cpp now it should be easier to edit.

--- src/libdsbs/clean_operation.cpp
+++ src/libdsbs/clean_operation.cpp
@@ -3,73 +3,49 @@
 
 namespace dsbs {
     namespace internal {
+        
+        void handleCleanOperations(dsbs::solution::Project& project, std::vector<std::filesystem::path>& paths) {
+            // Scan through objects
+            std::for_each(project.sources.begin(), project.sources.end(), [&](solution::Source& source) {
+                ::util::iterateDirectory(source.objDir, [&](std::filesystem::directory_entry entry) {
+                    if(entry.path().extension() == project.profile.fileExtensions.objectFileExtension) {
+                        paths.push_back(entry.path());
+                    }
+                });
+            });
+
+            if(project.type == solution::ProjectType::PT_EXECUTABLE) {
+                std::stringstream cb;
+                cb << project.binDir << project.name << project.profile.fileExtensions.binaryFileExtension;
+                paths.push_back(std::filesystem::path(cb.str()));
+            } else if(project.type == solution::ProjectType::PT_STATIC_LIB) {
+                std::stringstream cb;
+                cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
+                //cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
+                paths.push_back(std::filesystem::path(cb.str()));
+            } else if(project.type == solution::ProjectType::PT_SHARED_LIB) {
+                std::stringstream cb;
+                cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
+                //cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
+                paths.push_back(std::filesystem::path(cb.str()));
+                cb.str("");
+                cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
+                paths.push_back(std::filesystem::path(cb.str()));
+            }
+        }
+
         int cleanOperation(std::string projectName, solution::Solution& solution) {
             std::vector<std::filesystem::path> paths;
 
             if(!projectName.empty()) {
                 std::for_each(solution.projects.begin(), solution.projects.end(), [&](solution::Project& project) {
-
                     if(projectName == project.name) {
-                        // Scan through objects
-                        std::for_each(project.sources.begin(), project.sources.end(), [&](solution::Source& source) {
-                            ::util::iterateDirectory(source.objDir, [&](std::filesystem::directory_entry entry) {
-                                if(entry.path().extension() == project.profile.fileExtensions.objectFileExtension) {
-                                    paths.push_back(entry.path());
-                                }
-                            });
-                        });
-
-                        if(project.type == solution::ProjectType::PT_EXECUTABLE) {
-                            std::stringstream cb;
-                            cb << project.binDir << project.name << project.profile.fileExtensions.binaryFileExtension;
-                            paths.push_back(std::filesystem::path(cb.str()));
-
-                        } else if(project.type == solution::ProjectType::PT_STATIC_LIB) {
-                            std::stringstream cb;
-                            cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
-                            //cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
-                            paths.push_back(std::filesystem::path(cb.str()));
-                        } else if(project.type == solution::ProjectType::PT_SHARED_LIB) {
-                            std::stringstream cb;
-                            cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
-                            //cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
-                            paths.push_back(std::filesystem::path(cb.str()));
-                            cb.str("");
-                            cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
-                            paths.push_back(std::filesystem::path(cb.str()));
-                        }
-
+                        handleCleanOperations(project, paths);
                     }
                 });
             } else {
                 std::for_each(solution.projects.begin(), solution.projects.end(), [&](solution::Project& project) {
-                    // Scan through objects
-                    std::for_each(project.sources.begin(), project.sources.end(), [&](solution::Source& source) {
-                        ::util::iterateDirectory(source.objDir, [&](std::filesystem::directory_entry entry) {
-                            if(entry.path().extension() == project.profile.fileExtensions.objectFileExtension) {
-                                paths.push_back(entry.path());
-                            }
-                        });
-                    });
-
-                    if(project.type == solution::ProjectType::PT_EXECUTABLE) {
-                        std::stringstream cb;
-                        cb << project.binDir << project.name << project.profile.fileExtensions.binaryFileExtension;
-                        paths.push_back(std::filesystem::path(cb.str()));
-
-                    } else if(project.type == solution::ProjectType::PT_STATIC_LIB) {
-                        std::stringstream cb;
-                        cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
-                        paths.push_back(std::filesystem::path(cb.str()));
-                    } else if(project.type == solution::ProjectType::PT_SHARED_LIB) {
-                        std::stringstream cb;
-                        cb << project.binDir << project.name << project.profile.fileExtensions.staticLibExtension;
-                        //cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
-                        paths.push_back(std::filesystem::path(cb.str()));
-                        cb.str("");
-                        cb << project.binDir << project.name << project.profile.fileExtensions.sharedLibExtension;
-                        paths.push_back(std::filesystem::path(cb.str()));
-                    }
+                    handleCleanOperations(project, paths);
                 });
             }
 
