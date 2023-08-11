64ad495f7bc66f00be1e0fea895b79189a18c263
derf6060 <derf6060@derfnet.attlocal.net>
2023-08-11 02:05:45 -0500

    Got multi threaded working with windows version. Time to move one.

--- src/libdsbs/win/execute_command_list_win.cpp
+++ src/libdsbs/win/execute_command_list_win.cpp
@@ -3,34 +3,95 @@
 
 namespace dsbs {
     namespace internal {
-
-	/*
-	void executeThread(int n, const command::Command& c) {
-                 std::cout << "\t* " << c.command << "\n";
-                 std::string output = ::util::exec(c.command);
-               	 if(!output.empty()) {
-                       	 std::cout << output << "\n";
-		 }
-
+    
+    
+    	const static int MAX_THREADS = 8;
+    	
+    	struct Thread {
+    		HANDLE thread_id;
+    		std::vector<command::Command> commands;
+    	};
+	
+/*
+	void* compileThread(void* data) {
+		//std::cout << "Hello, World" << "\n";
+		
+		Thread* t = (Thread*)data;
+		
+		std::for_each(t->commands.begin(), t->commands.end(), [&](command::Command& command) {
+	    		std::cout << "\t* " << command.command << "\n";
+	    		std::string output = ::util::exec(command.command);
+	    		if(!output.empty()) {
+				std::cout << output << "\n";
+	    		}
+		});
 	}
-	*/
+*/
+	DWORD WINAPI compileThread(LPVOID data) {
+		//std::cout << "Hello, World" << "\n";
+		
+		Thread* t = (Thread*)data;
+		
+		std::for_each(t->commands.begin(), t->commands.end(), [&](command::Command& command) {
+	    		std::cout << "\t* " << command.command << "\n";
+	    		std::string output = ::util::exec(command.command);
+	    		if(!output.empty()) {
+				std::cout << output << "\n";
+	    		}
+		});
+	}
+	
         int executeCommandList(std::vector<command::Project>& projectCommandList, bool useMT) {
             std::for_each(projectCommandList.begin(), projectCommandList.end(), [&](command::Project& project) {
                 std::cout << "Building \""<< project.projectName <<"\"..." << "\n";
                 
 		// Execute Commands
 		if(useMT) {
-			std::cout << "Error: Current windows build can't use -useMT. Will need to figure out a better approach for this." << "\n";
+			//std::cout << "Error: Current windows build can't use -useMT. Will need to figure out a better approach for this." << "\n";
+			
+			
+			std::vector<Thread> threads;
+			threads.resize(MAX_THREADS);
+			
+			// Setup Data
+			for(int i = 0; i < project.commands.size(); i++) {
+				threads[i % MAX_THREADS].commands.push_back(project.commands[i]);
+			}
+			
+			for(int i = 0; i < threads.size(); i++) {
+				threads[i].thread_id = CreateThread(
+					nullptr,
+					0,
+					compileThread,
+					&threads[i],
+					0,
+					nullptr
+				);
+			}
+			
+			
+			for(int i = 0; i < threads.size(); i++) {
+				WaitForSingleObject(threads[i].thread_id, INFINITE);
+			}
+			
+			
+			// Cleanup Threads
+			for(int i = 0; i < threads.size(); i++) {
+				CloseHandle(threads[i].thread_id);
+			}
+			
+			//WaitForMultipleObjects(MAX_THREADS, 
 		}
 		
-		std::for_each(project.commands.begin(), project.commands.end(), [&](command::Command& command) {
-                    	std::cout << "\t* " << command.command << "\n";
-                    	std::string output = ::util::exec(command.command);
-                    	if(!output.empty()) {
-                        	std::cout << output << "\n";
-                    	}
-                });
-
+		else {
+			std::for_each(project.commands.begin(), project.commands.end(), [&](command::Command& command) {
+		            	std::cout << "\t* " << command.command << "\n";
+		            	std::string output = ::util::exec(command.command);
+		            	if(!output.empty()) {
+		                	std::cout << output << "\n";
+		            	}
+		        });
+		}
 		// Execute Project Command
               	std::cout << "\t* " << project.projectCommand.command << "\n";
                 std::string output = ::util::exec(project.projectCommand.command);
