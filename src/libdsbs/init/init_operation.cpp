de7ef0d062aeb3f3213564fedf6ca61818a5ef49
derf6060 <derf6060@derfnet.attlocal.net>
2023-08-11 02:55:33 -0500

    Implemented init operation. It will make it easier to manage projects.

--- src/libdsbs/init/init_operation.cpp
+++ src/libdsbs/init/init_operation.cpp
@@ -6,7 +6,45 @@ namespace dsbs {
 		namespace init {
 			int initOperation(solution::Solution& solution) {
 				// TODO ~ Work on initOperation
-				std::cout << "TODO ~ Work on initOperation" << "\n";
+				//std::cout << "TODO ~ Work on initOperation" << "\n";
+				
+				std::for_each(solution.projects.begin(), solution.projects.end(), [&](solution::Project& p) {
+					// Bin Dirs
+					if(!std::filesystem::exists(std::filesystem::path(p.binDir))) {
+						std::filesystem::create_directories(std::filesystem::path(p.binDir));
+					}
+					
+					// Sources
+					std::for_each(p.sources.begin(), p.sources.end(), [&](solution::Source& s) {
+						
+						// src-dir
+						if(!std::filesystem::exists(std::filesystem::path(s.srcDir))) {
+							std::filesystem::create_directories(std::filesystem::path(s.srcDir));
+						}
+						// cache dir
+						if(!std::filesystem::exists(std::filesystem::path(s.cacheDir))) {
+							std::filesystem::create_directories(std::filesystem::path(s.cacheDir));
+						}						
+						// obj dir
+						if(!std::filesystem::exists(std::filesystem::path(s.objDir))) {
+							std::filesystem::create_directories(std::filesystem::path(s.objDir));
+						}
+					});
+					
+					// Includes Dirs
+					std::for_each(p.includeDirs.begin(), p.includeDirs.end(), [&](std::string inc) {
+						if(!std::filesystem::exists(std::filesystem::path(inc))) {
+							std::filesystem::create_directories(std::filesystem::path(inc));
+						}
+					});
+					// Lib Dirs
+					std::for_each(p.libDirs.begin(), p.libDirs.end(), [&](std::string l) {
+						if(!std::filesystem::exists(std::filesystem::path(l))) {
+							std::filesystem::create_directories(std::filesystem::path(l));
+						}
+					});
+				});
+				
 				return 0;
 			}
 		}
